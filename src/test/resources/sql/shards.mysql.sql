6f9699f4a063e22bc591064a4c9b02c06cb0e1be
Feng Kuok <fengkuok@gmail.com>
2012-08-03 16:14:40 +0800

    更新数据库脚本

--- src/test/resources/sql/shards.mysql.sql
+++ src/test/resources/sql/shards.mysql.sql
@@ -1,6 +1,13 @@
-CREATE TABLE maker_test_user (
-   id int(11) NOT NULL,
+CREATE TABLE maker_shards_user (
+   id varchar(32) NOT NULL,
    username varchar(255) NOT NULL,
-   passworkd varchar(32) NOT NULL,
+   password varchar(32) NOT NULL,
    PRIMARY KEY (id)
- ) ENGINE=InnoDB DEFAULT CHARSET=utf8
+ ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+ 
+CREATE TABLE maker_shards_role(
+	id varchar(32)  NOT NULL,
+	name varchar(64) NOT NULL,
+	code varchar(64) NOT null,
+	PRIMARY KEY (id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
