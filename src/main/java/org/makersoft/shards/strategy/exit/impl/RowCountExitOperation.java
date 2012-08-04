2a66d69b84118847eb225bd2c782450639ca63e3
Feng Kuok <fengkuok@gmail.com>
2012-08-04 11:13:59 +0800

    update:方法改进

--- src/main/java/org/makersoft/shards/strategy/exit/impl/RowCountExitOperation.java
+++ src/main/java/org/makersoft/shards/strategy/exit/impl/RowCountExitOperation.java
@@ -1,5 +1,6 @@
 package org.makersoft.shards.strategy.exit.impl;
 
+import java.math.BigDecimal;
 import java.util.Collections;
 import java.util.List;
 
@@ -15,7 +16,24 @@ public class RowCountExitOperation implements ExitOperation {
 	public List<Object> apply(List<Object> result) {
 		List<Object> nonNullResults = ExitOperationUtils.getNonNullList(result);
 
-		return Collections.<Object>singletonList(nonNullResults.size());
+		return Collections.<Object>singletonList(this.getCount(nonNullResults));
+	}
+	
+	private Number getCount(List<Object> results) {
+		BigDecimal sum = new BigDecimal(0.0);
+		for (Object obj : results) {
+			sum = sum.add(new BigDecimal(obj.toString()));
+		}
+		
+		if(results != null){
+			Object obj = results.get(0);
+			if(obj instanceof Integer){
+				return sum.intValue();
+			}else if(obj instanceof Long){
+				return sum.longValue();
+			}
+		}
+		return sum;
 	}
 
 }
