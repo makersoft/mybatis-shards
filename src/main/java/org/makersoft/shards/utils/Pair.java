372390c41587483d24689adc9a7a9109041e4827
Feng Kuok <fengkuok@gmail.com>
2012-08-06 18:21:59 +0800

    bug fix:override hashCode method

--- src/main/java/org/makersoft/shards/utils/Pair.java
+++ src/main/java/org/makersoft/shards/utils/Pair.java
@@ -52,6 +52,15 @@ public class Pair<A, B> {
 	}
 
 	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((first == null) ? 0 : first.hashCode());
+		result = prime * result + ((second == null) ? 0 : second.hashCode());
+		return result;
+	}
+
+	@Override
 	public boolean equals(/* @Nullable */Object object) {
 		if (object instanceof Pair<?, ?>) {
 			Pair<?, ?> other = (Pair<?, ?>) object;
