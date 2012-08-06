7f3d2542ae17364b5a327c1dc22f1b7dd748a438
Feng Kuok <fengkuok@gmail.com>
2012-08-06 18:46:58 +0800

    来自springside的反射工具类.

---
+++ src/main/java/org/makersoft/shards/utils/ReflectionUtils.java
@@ -0,0 +1,76 @@
+package org.makersoft.shards.utils;
+
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+
+import org.apache.ibatis.logging.Log;
+import org.apache.ibatis.logging.LogFactory;
+
+/**
+ * 反射工具类.
+ * 
+ * 提供访问私有变量,获取泛型类型Class, 提取集合中元素的属性, 转换字符串到对象等Util函数.
+ * 
+ */
+public class ReflectionUtils {
+
+	private static final Log log = LogFactory.getLog(ReflectionUtils.class);
+	
+	private ReflectionUtils(){
+		
+	}
+	
+	/**
+	 * 通过反射, 获得Class定义中声明的父类的泛型参数的类型. 如无法找到, 返回Object.class. eg. public UserDao
+	 * extends HibernateDao<User>
+	 * 
+	 * @param clazz
+	 *            The class to introspect
+	 * @return the first generic declaration, or Object.class if cannot be
+	 *         determined
+	 */
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	public static <T> Class<T> getSuperClassGenricType(final Class clazz) {
+		return getSuperClassGenricType(clazz, 0);
+	}
+
+	/**
+	 * 通过反射, 获得Class定义中声明的父类的泛型参数的类型. 如无法找到, 返回Object.class.
+	 * 
+	 * 如public UserDao extends HibernateDao<User,Long>
+	 * 
+	 * @param clazz
+	 *            clazz The class to introspect
+	 * @param index
+	 *            the Index of the generic ddeclaration,start from 0.
+	 * @return the index generic declaration, or Object.class if cannot be
+	 *         determined
+	 */
+	@SuppressWarnings("rawtypes")
+	public static Class getSuperClassGenricType(final Class clazz,
+			final int index) {
+
+		Type genType = clazz.getGenericSuperclass();
+
+		if (!(genType instanceof ParameterizedType)) {
+			log.warn(clazz.getSimpleName()+ "'s superclass not ParameterizedType");
+			return Object.class;
+		}
+
+		Type[] params = ((ParameterizedType) genType).getActualTypeArguments();
+
+		if (index >= params.length || index < 0) {
+			log.warn("Index: " + index + ", Size of "
+					+ clazz.getSimpleName() + "'s Parameterized Type: "
+					+ params.length);
+			return Object.class;
+		}
+		if (!(params[index] instanceof Class)) {
+			log.warn(clazz.getSimpleName()
+					+ " not set the actual class on superclass generic parameter");
+			return Object.class;
+		}
+
+		return (Class) params[index];
+	}
+}
