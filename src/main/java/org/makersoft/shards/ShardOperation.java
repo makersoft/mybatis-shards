6ca3efcd3c07ea439cd4f11c54779cbfdd8c1a4a
Feng Kuok <fengkuok@gmail.com>
2012-08-06 15:09:54 +0800

    多线程优化：将ShardOperation改为虚拟分区级别的操作。

--- src/main/java/org/makersoft/shards/ShardOperation.java
+++ src/main/java/org/makersoft/shards/ShardOperation.java
@@ -1,15 +1,18 @@
 package org.makersoft.shards;
 
+import org.apache.ibatis.session.SqlSession;
+
 /**
  * Simple interface used to reference something we can do against a {@link Shard}.
  */
 public interface ShardOperation<T> {
+
 	/**
-	 * @param shard
-	 *            the shard to execute against
-	 * @return the result of the operation
+	 * @param session	the seesion of the shard to execute	
+	 * @param shardId	the shardId to execute against
+	 * @return	the result of the operation
 	 */
-	T execute(Shard shard);
+	T execute(SqlSession session, ShardId shardId);
 
 	/**
 	 * @return the name of the operation (useful for logging and debugging)
