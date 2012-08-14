acff580420fce9d53626f023802d741685112af5
Feng Kuok <fengkuok@gmail.com>
2012-08-14 15:50:43 +0800

    update mlti datasource transaction manager unit test.

--- src/test/java/org/makersoft/shards/unit/transaction/MultipleDataSourcesTransactionManagerTests.java
+++ src/test/java/org/makersoft/shards/unit/transaction/MultipleDataSourcesTransactionManagerTests.java
@@ -14,7 +14,7 @@ import org.springframework.test.context.transaction.TransactionConfiguration;
 import org.springframework.transaction.annotation.Transactional;
 
 /**
- * 
+ * 多数据源事务管理器单元测试
  */
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = { "classpath:applicationContext.xml" })
@@ -30,96 +30,16 @@ public class MultipleDataSourcesTransactionManagerTests {
 	@Qualifier("jdbcTemplate_2")
 	private JdbcTemplate jdbcTemplate_2;
 	
-//	@Autowired
-//	@Qualifier("jdbcTemplate_3")
-//	private JdbcTemplate jdbcTemplate_3;
-	
 	@Test
 	@Transactional(rollbackFor={Exception.class,RuntimeException.class})
 	@Rollback
-	public void testOfferCreationOnMultipleShardsWithTransactionRollback() throws Exception{
+	public void testTransactionCommitAndRollback() throws Exception{
 
-//		User user1 = new User(1L,"makersoft","makersoft");
-//		User user2 = new User(1L,"guofeng","guofeng");
-							
-		int affectedrows = jdbcTemplate_1.update("insert into maker_test_user (id,username,password) values (1,'makersoft','makersoft')");
+		int affectedrows = jdbcTemplate_1.update("insert into maker_shards_user (id,username,password,gender) values (1,'makersoft','makersoft',1)");
 		Assert.assertEquals(affectedrows, 1);
-//		jdbcTemplate_1.update("insert into maker_test_user (id,username,password) values (1,'makersoft','makersoft')");
-		affectedrows = jdbcTemplate_2.update("insert into maker_test_user (id,username,password) values (1,'makersoft','makersoft')");
-//		jdbcTemplate_2.update("insert into maker_test_user (id,username,password) values (1,'makersoft','makersoft')");
+		affectedrows = jdbcTemplate_2.update("insert into maker_shards_user (id,username,password,gender) values (1,'makersoft','makersoft',0)");
 		Assert.assertEquals(affectedrows, 1);
-//		if(true){
-//			throw new RuntimeException();
-//		}
 
 	}
-
-//	public void testOfferCreationOnMultipleShardsWithNormallyOfferService() {
-//		String selectSqlActionTwo = "com.alibaba.cobar.client.entities.Offer.findByMemberId";
-//
-//		for (Long mid : memberIds) {
-//			Offer parameter = new Offer();
-//			parameter.setMemberId(mid);
-//			Offer offer = (Offer) getSqlMapClientTemplate().queryForObject(
-//					selectSqlActionTwo, parameter);
-//			assertNull(offer);
-//		}
-//
-//		((IOfferService) getApplicationContext().getBean("normalOfferService"))
-//				.createOffersInBatch(createOffersWithMemberIdsFrom(memberIds));
-//
-//		for (Long mid : memberIds) {
-//			Offer parameter = new Offer();
-//			parameter.setMemberId(mid);
-//			Offer offer = (Offer) getSqlMapClientTemplate().queryForObject(
-//					selectSqlActionTwo, parameter);
-//			assertNotNull(offer);
-//			assertEquals(mid, offer.getMemberId());
-//		}
-//	}
-//
-//	/**
-//	 * need data stores that support transaction to test this behavior.
-//	 */
-//	public void testOfferCreationOnMultipleShardsWithAbnormalOfferService() {
-//		String selectSqlActionTwo = "com.alibaba.cobar.client.entities.Offer.findByMemberId";
-//
-//		for (Long mid : memberIds) {
-//			Offer parameter = new Offer();
-//			parameter.setMemberId(mid);
-//			Offer offer = (Offer) getSqlMapClientTemplate().queryForObject(
-//					selectSqlActionTwo, parameter);
-//			assertNull(offer);
-//		}
-//
-//		try {
-//			Object offerService = getApplicationContext().getBean(
-//					"abnormalOfferService");
-//			assertTrue(offerService instanceof Proxy);
-//			((IOfferService) offerService)
-//					.createOffersInBatch(createOffersWithMemberIdsFrom(memberIds));
-//			fail();
-//		} catch (RuntimeException e) {
-//			// pass
-//		}
-//
-//		for (Long mid : memberIds) {
-//			Offer parameter = new Offer();
-//			parameter.setMemberId(mid);
-//			assertNull(getSqlMapClientTemplate().queryForObject(
-//					selectSqlActionTwo, parameter));
-//		}
-//	}
-//
-//	public List<Offer> createOffersWithMemberIdsFrom(Long[] mids) {
-//		List<Offer> offers = new ArrayList<Offer>();
-//		for (Long mid : mids) {
-//			Offer offer = new Offer();
-//			offer.setGmtUpdated(new Date());
-//			offer.setMemberId(mid);
-//			offer.setSubject("anything");
-//			offers.add(offer);
-//		}
-//		return offers;
-//	}
+	
 }
