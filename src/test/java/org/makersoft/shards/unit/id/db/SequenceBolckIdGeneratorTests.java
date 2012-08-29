9d9727591882c14a819d69ae789660ad7ecbe5b0
Feng Kuok <fengkuok@gmail.com>
2012-08-29 17:46:16 +0800

    add unit test.

---
+++ src/test/java/org/makersoft/shards/unit/id/db/SequenceBolckIdGeneratorTests.java
@@ -0,0 +1,19 @@
+/*
+ * @(#)SequenceBolckIdGeneratorTests.java 2012-8-22 下午4:24:53
+ *
+ * Copyright (c) 2011-2012 Makersoft.org all rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ *
+ */
+package org.makersoft.shards.unit.id.db;
+
+/**
+ * unit test.
+ * 
+ * @version 2012-8-22 下午4:24:53
+ * @author Feng Kuok
+ */
+public class SequenceBolckIdGeneratorTests {
+
+}
