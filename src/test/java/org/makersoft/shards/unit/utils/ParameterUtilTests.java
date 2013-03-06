3b53858d573c1cccedf96208abe5590ba2308582
fengkuok <fengkuok@gmail.com>
2013-03-06 17:44:00 +0800

    fixed:junit相关包导入warning

--- src/test/java/org/makersoft/shards/unit/utils/ParameterUtilTests.java
+++ src/test/java/org/makersoft/shards/unit/utils/ParameterUtilTests.java
@@ -14,8 +14,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import junit.framework.Assert;
-
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.makersoft.shards.ShardId;
