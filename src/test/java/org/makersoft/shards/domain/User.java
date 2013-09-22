1e3811f3c5acd0fe3b759ce86f72a6032692d6f6
Feng Kuok <fengkuok@gmail.com>
2013-09-22 10:42:52 +0800

    add copy right

--- src/test/java/org/makersoft/shards/domain/User.java
+++ src/test/java/org/makersoft/shards/domain/User.java
@@ -70,4 +70,43 @@ public class User extends AbstractShardEntity {
 		this.gender = gender;
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + age;
+		result = prime * result + gender;
+		result = prime * result
+				+ ((password == null) ? 0 : password.hashCode());
+		result = prime * result
+				+ ((username == null) ? 0 : username.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		if (age != other.age)
+			return false;
+		if (gender != other.gender)
+			return false;
+		if (password == null) {
+			if (other.password != null)
+				return false;
+		} else if (!password.equals(other.password))
+			return false;
+		if (username == null) {
+			if (other.username != null)
+				return false;
+		} else if (!username.equals(other.username))
+			return false;
+		return true;
+	}
+
 }
