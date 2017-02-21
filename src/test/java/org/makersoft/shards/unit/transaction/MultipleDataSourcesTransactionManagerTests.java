8f509bf242da0a0e11ea99d181e6e326d61f51fc
马翼超 <mycsoft@qq.com>
2017-02-21 11:30:26 +0800

    修正测试脚本,ID应该是文字类型.

--- src/test/java/org/makersoft/shards/unit/transaction/MultipleDataSourcesTransactionManagerTests.java
+++ src/test/java/org/makersoft/shards/unit/transaction/MultipleDataSourcesTransactionManagerTests.java
@@ -34,10 +34,10 @@ public class MultipleDataSourcesTransactionManagerTests {
 	@Rollback
 	public void test_transaction_commit_and_rollback() throws Exception{
 
-		int affectedrows = jdbcTemplate_1.update("insert into maker_shards_user (id,username,password,age,gender) values (1,'makersoft','makersoft',23,1)");
+		int affectedrows = jdbcTemplate_1.update("insert into maker_shards_user (id,username,password,age,gender) values ('1','makersoft','makersoft',23,1)");
 		Assert.assertEquals(affectedrows, 1);
 
-		affectedrows = jdbcTemplate_2.update("insert into maker_shards_user (id,username,password,age,gender) values (1,'makersoft','makersoft',22,0)");
+		affectedrows = jdbcTemplate_2.update("insert into maker_shards_user (id,username,password,age,gender) values ('1','makersoft','makersoft',22,0)");
 		Assert.assertEquals(affectedrows, 1);
 
 	}
