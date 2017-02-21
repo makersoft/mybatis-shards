bcb9dc356b106b37b8bbb60d4dace72e83e9ecef
马翼超 <mycsoft@qq.com>
2017-02-21 18:04:33 +0800

    上传e企学测试代码:实现添加平台企业

--- src/test/resources/databases/sql/derby/shards.schema.sql
+++ src/test/resources/databases/sql/derby/shards.schema.sql
@@ -14,7 +14,7 @@ CREATE TABLE maker_shards_role(
 	PRIMARY KEY (id)
 ) ;
 
-CREATE TABLE maker_shards_user (
+CREATE TABLE easyxue_user (
    id varchar(32) NOT NULL,
    username varchar(255) NOT NULL,
    password varchar(32) NOT NULL,
@@ -23,9 +23,10 @@ CREATE TABLE maker_shards_user (
    PRIMARY KEY (id)
  );
  
-CREATE TABLE maker_shards_role(
+CREATE TABLE easyxue_company(
 	id varchar(32)  NOT NULL,
 	name varchar(64) NOT NULL,
-	code varchar(64) NOT null,
+	creatorId varchar(64) NOT null,
+	dbkey varchar(32) NOT null,
 	PRIMARY KEY (id)
 ) ;
